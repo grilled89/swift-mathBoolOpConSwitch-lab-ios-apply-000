500e5fb6c303df66c7ac9210260f4e30d6215a6b
Ryan King <rk13@Ryans-MacBook-Pro.local>
2017-01-10 14:58:20 -0500

    complete

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -14,8 +14,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    
-    // Implement your functions here!
-    
+ // 1)
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        if (a + b + c) / 3 > 75 {
+            return true }
+        else {
+            return false
+        }
+    }
+//2) 
 
+    func passwordCombo(username: String, password: Int) -> String {
+        if username == "Jerry" && password % 3 == 0  {
+            return "Welcome!"
+        }
+        else if username == "Elaine" && password % 3 == 0 {
+            return "Welcome!"
+        }
+        else if username == "Michael" && password % 3 == 0 {
+            return "Welcome!"
+        }
+        else { return "Access Denied" }
+        }
+//3 
+    func describe(emoji: String) -> String {
+        if emoji == "💋" {
+            return "Kiss"
+        }
+        else if emoji == "🐈" {
+            return "Cat"
+        }
+        else if emoji == "🐢" {
+            return "Turtle"
+        }
+        else if emoji == "🍕" {
+            return "Pizza"
+        }
+        else if emoji == "👻" {
+            return "Ghost"
+        }
+        else { return "Unknown"
+        }
+}
 }
